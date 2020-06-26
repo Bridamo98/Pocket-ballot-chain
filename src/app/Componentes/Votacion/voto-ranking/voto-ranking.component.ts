8a8b668e1647df65540346e7b19e7339642952c0
SantiagoChaparro <41376467+SantiagoChaparro@users.noreply.github.com>
2020-06-25 21:14:29 -0500

    votacion por ranking terminada

--- src/app/Componentes/Votacion/voto-ranking/voto-ranking.component.ts
+++ src/app/Componentes/Votacion/voto-ranking/voto-ranking.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Usuario } from '../../../Modelo/Usuario';
-
+import { Votacion } from '../../../Modelo/Votacion';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 @Component({
   selector: 'app-voto-ranking',
   templateUrl: './voto-ranking.component.html',
@@ -9,12 +10,23 @@ import { Usuario } from '../../../Modelo/Usuario';
 export class VotoRankingComponent implements OnInit {
 
   constructor() { }
-
-  //Quemar
+  tituloVotacion: string="Titulo votacion"; //El modelo de votacion aun no tiene titulo
+    //Quemar
   candidatos: string[]=['Santiago', 'Brandonn', 'Diego', 'Briam'];
   ngOnInit(): void {
   	console.log(this.candidatos);
 
+  }
+  onDropped(event: CdkDragDrop<any>){
+  	console.log(event);
+  	const before=event.previousIndex;
+  	const actual= event.currentIndex;
+  	moveItemInArray(this.candidatos, before, actual);
+  	//console.log(this.candidatos);
+  }
+  registrarse()
+  {
+  	console.log(this.candidatos);
   }
 
 
