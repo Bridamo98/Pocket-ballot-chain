41225fdc230986936fdd701522986e571abd4709
barajas-d <diegobarajas102030@gmail.com>
2020-07-02 21:44:39 -0500

    a

--- src/app/Componentes/Votacion/votacion-crear-informacion/votacion-crear-informacion.component.ts
+++ src/app/Componentes/Votacion/votacion-crear-informacion/votacion-crear-informacion.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Opcion } from 'src/app/Modelo/Opcion';
 import { NgbInputDatepicker, NgbDatepicker } from '@ng-bootstrap/ng-bootstrap';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-votacion-crear-informacion',
@@ -8,6 +9,9 @@ import { NgbInputDatepicker, NgbDatepicker } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./votacion-crear-informacion.component.css']
 })
 export class VotacionCrearInformacionComponent implements OnInit {
+
+  tipo: string;
+  cantiVotos: number;
 
   opciones: Opcion[] = [
     {id:1, nombre:"uno", descripcion:"descripcion uno"}, 
@@ -23,9 +27,20 @@ export class VotacionCrearInformacionComponent implements OnInit {
     {id:2, nombre:'dos', descripcion:'descripcion dos'}, 
     {id:3, nombre:'tres', descripcion:'descripcion tres'}];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private rutaActiva: ActivatedRoute, private router: Router) {
+    this.tipo = this.rutaActiva.snapshot.params.tipo;
+    this.cantiVotos = this.rutaActiva.snapshot.params.cantiVotos;
   }
 
+  ngOnInit(): void {
+    if(this.cantiVotos == undefined){
+      console.log("Esta undefinido");
+    }
+    else{
+      console.log(this.cantiVotos);
+    }
+    if(this.tipo !== "Popular" && this.tipo !== "Ranking" && this.tipo !== "Clasificacion"){
+      this.router.navigate(['CrearVotacion']);
+    }
+  }
 }
