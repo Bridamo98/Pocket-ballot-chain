568decb99aeb2a65736828dbf4440006ef616c17
SantiagoChaparro <41376467+SantiagoChaparro@users.noreply.github.com>
2020-07-05 15:16:33 -0500

    Primer commit

--- src/app/Servicios/Usuario/usuario.service.ts
+++ src/app/Servicios/Usuario/usuario.service.ts
@@ -9,14 +9,16 @@ export class UsuarioService {
 	URLbase = 'http://localhost:3000';
 	private _httpHandler: HttpHandler;
 	httpClient: HttpClient;
-  constructor( http: HttpClient) { 
+  constructor(private http: HttpClient) { 
   	this.httpClient= http
   }
   addUsuario(usuario: Usuario){
-    //var json = JSON.stringify(usuario); //Esto jode el post
-    //console.log(json);
     return this.httpClient.post(this.URLbase+'/usuarioAdd', usuario).toPromise().then(data=> {
 			console.log(data);
 		});;
+  }
+  iniciarSesion(usuario: Usuario)
+  {
+        return this.http.post<any>('${environment.serverUrl}/iniciarSesion', usuario);
   }
 }
