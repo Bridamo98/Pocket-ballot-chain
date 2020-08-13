c9fc42af6ec982711682df593b65563779e82dd2
Briam Daniel Agudelo Mogollón <bridamo.98@gmail.com>
2020-08-12 20:53:50 -0500

    integracion completa

--- src/app/Componentes/Validador/validador-postularse/validador-postularse.component.ts
+++ src/app/Componentes/Validador/validador-postularse/validador-postularse.component.ts
@@ -3,7 +3,9 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder } from '@angular/forms';
 import { Usuario } from 'src/app/Modelo/Usuario';
 
-declare var Peer: any;
+declare var inicializar: any;
+declare var establecerConexion: any;
+declare var enviarMensaje: any;
 
 @Component({
   selector: 'app-validador-postularse',
@@ -14,9 +16,7 @@ export class ValidadorPostularseComponent implements OnInit {
 
   usuario: Usuario = new Usuario('', 0, '', '');
 
-  peer;
-  conn;
-  peer_id;
+  #peer;
   otro_peer_id;
   msj;
 
@@ -27,60 +27,19 @@ export class ValidadorPostularseComponent implements OnInit {
   ) {  }
 
   ngOnInit(): void {
-
-    this.peer = new Peer({
-      host: location.hostname,
-      port: 9000,
-      path: '/peerjs'
-    });
-    setTimeout(() => {
-      this.peer_id = this.peer.id
-    }, 3000);
-    
-
-    this.peer.on('connection', function (connection) {
-      this.conn = connection;
-      this.peer_id = connection.peer;
-      console.log('peer connection: ' + this.peer_id);
-      this.conn.on('data', function (data) {
-        // Will print 'this is a test'
-        console.log(data);
-      });
-    });
-
-    this.peer.on('error', function (err) {
-      alert('Error: ' + err);
-      console.log(err);
-    })
+    inicializar();
   }
 
   serValidador(): void {
     console.log('serValidador');
-    //this.router.navigate(['*/'+this.usuario.nombre]);
   }
 
-  establecerConexion():boolean {
-    
-    if (this.otro_peer_id) {
-      this.peer_id = this.otro_peer_id;
-      this.conn = this.peer.connect(this.peer_id);
-      this.conn.on('open', function () {
-        this.conn.send('confirmacion');
-        console.log('Enviando confirmacion');
-      });
-      this.conn.on('data', function (data) {
-        console.log(data);
-      });
-    } else {
-      alert('Ingresa un peerId');
-      return false;
-    }
+  establecerConexion():void {
+    establecerConexion(this.otro_peer_id);
   }
 
   enviarMensaje():void{
-    
-    console.log('Enviando mensaje ' + this.msj);
-    this.conn.send(this.msj); 
+    enviarMensaje(this.msj);
   }
 
 }
